be9439ab4ce9a614f8b3a254bed52b97ef5119e6
Khoa Nguyen <nguyenkhoa0721@gmail.com>
2019-11-10 15:20:38 +0700

    none

---
+++ main.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import gzip
+import pickle
+import histogram
+def load_mnist(path,kind='train'):
+    labels_path = os.path.join(path, '%s-labels-idx1-ubyte.gz' % kind)
+    images_path = os.path.join(path, '%s-images-idx3-ubyte.gz' % kind)
+    with gzip.open(labels_path, 'rb') as lbpath:
+        lbpath.read(8)
+        buffer = lbpath.read()
+        labels = np.frombuffer(buffer, dtype=np.uint8)
+    with gzip.open(images_path, 'rb') as imgpath:
+        imgpath.read(16)
+        buffer = imgpath.read()
+        images = np.frombuffer(buffer, dtype=np.uint8).reshape(len(labels), 28,
+        28).astype(np.float64)
+    return images, labels
+
+X_train, y_train = load_mnist('data/', kind='train')
+print('Rows: %d, columns: %d' % (X_train.shape[0], X_train.shape[1]))
+fig, ax = plt.subplots(nrows=4, ncols=5, sharex=True, sharey=True,)
+ax = ax.flatten()
+
+#file = open('histogram.pickle', 'rb')
+#his=pickle.load(file)
+his=histogram.hiscal(X_train)
+print(his.shape[0])
+print(his.shape[1])
+#xuat hin
+for i in range(10):
+    img = X_train[i]
+    ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
+
+
